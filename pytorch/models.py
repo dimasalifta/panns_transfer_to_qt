9877f06cb7a23a0adf19ef86622eaf2e809105c5
sshrmn7889 <sshrmn7889@gmail.com>
2024-07-22 14:00:15 +0700

    add

--- pytorch/models.py
+++ pytorch/models.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from pytorch_utils import do_mixup, interpolate, pad_framewise_output, Interpolator
+from pytorch_utils import do_mixup
  
 
 def init_layer(layer):
@@ -159,111 +159,6 @@ class Cnn14(nn.Module):
         return output_dict
 
 
-class Cnn14_DecisionLevelMax(nn.Module):
-    def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, 
-        fmax, classes_num, interpolate_mode='nearest'):
-        
-        super(Cnn14_DecisionLevelMax, self).__init__()
-
-        window = 'hann'
-        center = True
-        pad_mode = 'reflect'
-        ref = 1.0
-        amin = 1e-10
-        top_db = None
-        self.interpolate_ratio = 32     # Downsampled ratio
-
-        # Spectrogram extractor
-        self.spectrogram_extractor = Spectrogram(n_fft=window_size, hop_length=hop_size, 
-            win_length=window_size, window=window, center=center, pad_mode=pad_mode, 
-            freeze_parameters=True)
-
-        # Logmel feature extractor
-        self.logmel_extractor = LogmelFilterBank(sr=sample_rate, n_fft=window_size, 
-            n_mels=mel_bins, fmin=fmin, fmax=fmax, ref=ref, amin=amin, top_db=top_db, 
-            freeze_parameters=True)
-
-        # Spec augmenter
-        self.spec_augmenter = SpecAugmentation(time_drop_width=64, time_stripes_num=2, 
-            freq_drop_width=8, freq_stripes_num=2)
-
-        self.bn0 = nn.BatchNorm2d(64)
-
-        self.conv_block1 = ConvBlock(in_channels=1, out_channels=64)
-        self.conv_block2 = ConvBlock(in_channels=64, out_channels=128)
-        self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
-        self.conv_block4 = ConvBlock(in_channels=256, out_channels=512)
-        self.conv_block5 = ConvBlock(in_channels=512, out_channels=1024)
-        self.conv_block6 = ConvBlock(in_channels=1024, out_channels=2048)
-
-        self.fc1 = nn.Linear(2048, 2048, bias=True)
-        self.fc_audioset = nn.Linear(2048, classes_num, bias=True)
-
-        self.interpolator = Interpolator(
-            ratio=self.interpolate_ratio, 
-            interpolate_mode=interpolate_mode
-        )
-        
-        self.init_weight()
-
-    def init_weight(self):
-        init_bn(self.bn0)
-        init_layer(self.fc1)
-        init_layer(self.fc_audioset)
- 
-    def forward(self, input, mixup_lambda=None):
-        """
-        Input: (batch_size, data_length)"""
-
-        x = self.spectrogram_extractor(input)   # (batch_size, 1, time_steps, freq_bins)
-        x = self.logmel_extractor(x)    # (batch_size, 1, time_steps, mel_bins)
-
-        frames_num = x.shape[2]
-        
-        x = x.transpose(1, 3)
-        x = self.bn0(x)
-        x = x.transpose(1, 3)
-        
-        if self.training:
-            x = self.spec_augmenter(x)
-
-        # Mixup on spectrogram
-        if self.training and mixup_lambda is not None:
-            x = do_mixup(x, mixup_lambda)
-
-        x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block4(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block5(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block6(x, pool_size=(1, 1), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = torch.mean(x, dim=3)
-        
-        x1 = F.max_pool1d(x, kernel_size=3, stride=1, padding=1)
-        x2 = F.avg_pool1d(x, kernel_size=3, stride=1, padding=1)
-        x = x1 + x2
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = x.transpose(1, 2)
-        x = F.relu_(self.fc1(x))
-        x = F.dropout(x, p=0.5, training=self.training)
-        segmentwise_output = torch.sigmoid(self.fc_audioset(x))
-        (clipwise_output, _) = torch.max(segmentwise_output, dim=1)
-
-        # Get framewise output
-        framewise_output = self.interpolator(segmentwise_output)
-        framewise_output = pad_framewise_output(framewise_output, frames_num)
-
-        output_dict = {'framewise_output': framewise_output, 
-            'clipwise_output': clipwise_output}
-
-        return output_dict
-
 class Transfer_Cnn14(nn.Module):
     def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, 
         fmax, classes_num, freeze_base):
@@ -299,46 +194,6 @@ class Transfer_Cnn14(nn.Module):
         embedding = output_dict['embedding']
 
         clipwise_output =  torch.log_softmax(self.fc_transfer(embedding), dim=-1)
-        output_dict['clipwise_output'] = clipwise_output
- 
-        return output_dict
-    
-
-class Transfer_Cnn14_detect(nn.Module):
-    def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, 
-        fmax, classes_num, interpolate_mode='nearest'):
-        """Classifier for a new task using pretrained Cnn14 as a sub module.
-        """
-        super(Transfer_Cnn14_detect, self).__init__()
-        audioset_classes_num = 527
-        
-        self.base = Cnn14_DecisionLevelMax(sample_rate, window_size, hop_size, mel_bins, fmin, 
-            fmax, audioset_classes_num, interpolate_mode)
-
-        # Transfer to another task layer
-        self.fc_transfer = nn.Linear(2048, classes_num, bias=True)
-
-        # if freeze_base:
-        #     # Freeze AudioSet pretrained layers
-        #     for param in self.base.parameters():
-        #         param.requires_grad = False
-
-        self.init_weights()
-
-    def init_weights(self):
-        init_layer(self.fc_transfer)
-
-    def load_from_pretrain(self, pretrained_checkpoint_path):
-        checkpoint = torch.load(pretrained_checkpoint_path)
-        self.base.load_state_dict(checkpoint['model'])
-
-    def forward(self, input, mixup_lambda=None):
-        """Input: (batch_size, data_length)
-        """
-        output_dict = self.base(input, mixup_lambda)
-        framewise_output = output_dict['framewise_output']
-
-        clipwise_output =  torch.log_softmax(self.fc_transfer(framewise_output), dim=-1)
         output_dict['clipwise_output'] = clipwise_output
  
         return output_dict
